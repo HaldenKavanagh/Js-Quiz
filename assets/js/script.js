2fdab02ca7cbcf63ff0018d22b88c9ea0f2ee107
Halden Kavanagh <haldenkav@gmail.com>
2023-11-30 21:54:41 -0700

    added game over screen and question arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
 // const
 
-const startButton = document.getElementById("start-btn");
-
-// variable declorations
+var startButton = document.getElementById("start-btn");
+var questionContainer = document.getElementById("question-container");
+var buttonContainer = document.getElementById("answer-buttons");
+var gameWindow = document.getElementById("game-container");
 var timerEl = document.getElementById("countdown");
 var question1 = document.createElement("div");
-// timer value(seconds)
+var questionEl = document.getElementById("question");
+var buttonsEl = document.getElementById("btn");
 var startTime = 5;
-var gameSeconds = 60;
+var gameSeconds = 10;
 
+var questions = [
+  {
+    question: "what is the correct syntax for a variable?",
+    choices: ["variable() {}", "var()", "var.set()", "var = ''"],
+    answer: "var = ''",
+  },
+  {
+    question: "Wich answer is a boolian?",
+    choices: ["yes", "no", "false", "22.5"],
+    answer: "false",
+  },
+  {
+    question: "what is a string?",
+    choices: [
+      "a piece of text wrapped in quotes",
+      "any number",
+      "any number with a decimal",
+      "a function including if AND then statements",
+    ],
+    answer: "a piece of text wrapped in quotes",
+  },
+];
 // event listeners
 
 startButton.addEventListener("click", startGame);
@@ -16,31 +40,47 @@ startButton.addEventListener("click", startGame);
 // game function
 
 function startGame() {
+  startButton.classList.add("hide");
   countdown();
-  function countdown() {
-    var timeInterval = setInterval(function () {
-      startTime--;
-      timerEl.textContent = startTime + " seconds left...";
+}
+function showQuestion() {
+  questionEl.innerText = questions[0].question;
+}
 
-      if (startTime === 0) {
-        timerEl.textContent = "";
-        clearInterval(timeInterval);
-        gameTime();
-      }
-    }, 1000);
-  }
-  function gameTime() {
-    var timeInterval = setInterval(function () {
-      gameSeconds--;
-      timerEl.textContent = gameSeconds + " seconds left...";
-      if (gameSeconds > 0) {
-        console.log("yaaay");
-      }
+function countdown() {
+  var timeInterval = setInterval(function () {
+    startTime--;
+    timerEl.textContent = startTime + " seconds left...";
 
-      if (gameSeconds === 0) {
-        timerEl.textContent = "";
-        clearInterval(timeInterval);
-      }
-    }, 1000);
-  }
+    if (startTime === 0) {
+      timerEl.textContent = "";
+      questionContainer.classList.remove("hide");
+      clearInterval(timeInterval);
+      gameTime();
+      showQuestion();
+    }
+  }, 1000);
+}
+function gameTime() {
+  var timeInterval = setInterval(function () {
+    gameSeconds--;
+    timerEl.textContent = gameSeconds + " seconds left...";
+    if (gameSeconds > 0) {
+    }
+
+    if (gameSeconds === 0) {
+      timerEl.textContent = "";
+      clearInterval(timeInterval);
+      gameOver();
+    }
+  }, 1000);
+}
+
+function gameOver() {
+  timerEl.textContent = "You did it!";
+  questionEl.textContent = "Enter your initials to join the leaderboard";
+  var initialsInput = document.createElement("input");
+  initialsInput.setAttribute = ("type", "text");
+  initialsInput.setAttribute = ("placeholder", "Enter your initials here");
+  buttonContainer.appendChild(initialsInput);
 }
