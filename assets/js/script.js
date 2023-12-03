d376cb4392d79dbbfc9262d4a13408bd4c8d2805
Halden Kavanagh <haldenkav@gmail.com>
2023-12-03 01:34:54 -0700

    rewrote js, fixed quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,123 +1,84 @@
 // variable declorations
 
+var quizContainer = document.getElementById("quiz");
 var startButton = document.getElementById("start-btn");
-var nextButton = document.getElementById("next-btn");
-var questionContainer = document.getElementById("question-container");
-var buttonContainer = document.getElementById("answer-buttons");
-var gameWindow = document.getElementById("game-container");
+var questionContainer = document.getElementById("question");
+var optionsContainer = document.getElementById("options");
 var timerEl = document.getElementById("countdown");
-var question1 = document.createElement("div");
-var questionEl = document.getElementById("question");
-var buttonsEl = document.getElementById("btn");
 var startTime = 5;
 var gameSeconds = 60;
+startButton.addEventListener("click", startGame);
 
-var shuffledQuestions, currentQuestionIndex;
-
-var questions = [
+const questions = [
   {
-    question: "what is the correct syntax for a variable?",
-    answers: [
-      { text: "variable() {}", correct: false },
-      { text: "var()", correct: false },
-      { text: "var.set()", correct: false },
-      { text: "var = ''", correct: true },
+    question: "What is the world's largest rodent?",
+    options: [
+      "Giant Anteater",
+      "Capybara",
+      "African muskrat",
+      "New York City subway rats",
     ],
+    correctAnswer: "Capybara",
   },
   {
-    question: "Wich answer is a boolian?",
-    answers: [
-      { text: "yes", correct: false },
-      { text: "no", correct: false },
-      { text: "22.5", correct: false },
-      { text: "false", correct: true },
-    ],
+    question: "What is the imulsifier used to make a demi glaze?",
+    options: ["roux", "lecithin", "corn startch", "gelatin"],
+    correctAnswer: "gelatin",
   },
   {
-    question: "what is a string?",
-    answers: [
-      { text: "a piece of text wrapped in quotes", correct: true },
-      { text: "any number", correct: false },
-      { text: "any number with a decimal", correct: false },
-      { text: "a function including if AND then statements", correct: false },
+    question: "Who are the members of a tribe called quest?",
+    options: [
+      "Biggie and tupac",
+      "Lauren hill, pras, and Wyclef Jean",
+      "Q-tip and Phife Dawg",
+      "Nas and MF DOOM",
     ],
+    correctAnswer: "Q-tip and Phife Dawg",
+  },
+  {
+    question: "What is the most shallow ocean in the world?",
+    options: ["Pacific", "Antarctic", "Indian", "Atlantic"],
+    correctAnswer: "Antarctic",
   },
 ];
 
-// event listeners
-
-startButton.addEventListener("click", startGame);
-nextButton.addEventListener("click", () => {
-  currentQuestionIndex++;
-  setNextQuestion();
-});
-
-// game function
+let currentQuestionIndex = 0;
 
 function startGame() {
   startButton.classList.add("hide");
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0;
-
   countdown();
 }
 
-function setNextQuestion() {
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-  questionEl.innerText = question.question;
-  question.answers.forEach((answer) => {
-    var button = document.createElement("button");
-    button.innerText = answer.text;
-    button.classList.add("btn");
-    if (answer.correct) {
-      button.dataset.correct = answer.correct;
-    }
-    button.addEventListener("click", selectAnswer);
-    buttonContainer.appendChild(button);
+function displayQuestion() {
+  const currentQuestion = questions[currentQuestionIndex];
+  questionContainer.textContent = currentQuestion.question;
+  optionsContainer.innerHTML = "";
+  currentQuestion.options.forEach((option, index) => {
+    const li = document.createElement("li");
+    li.textContent = option;
+    li.addEventListener("click", () => checkAnswer(index));
+    optionsContainer.appendChild(li);
   });
 }
 
-function resetState() {
-  clearStatusClass(document.body);
-  nextButton.classList.add("hide");
-  while (buttonContainer.firstChild) {
-    buttonContainer.removeChild(buttonContainer.firstChild);
-  }
-}
+function checkAnswer(selectedIndex) {
+  const currentQuestion = questions[currentQuestionIndex];
+  const selectedOption = currentQuestion.options[selectedIndex];
 
-function selectAnswer(e) {
-  var selectedButton = e.target;
-  var correct = selectedButton.dataset.correct;
-  setStatusClass(document.body, correct);
-  Array.from(buttonContainer.children).forEach((button) => {
-    setStatusClass(button, button.dataset.correct);
-  });
-  nextButton.classList.remove("hide");
-  if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove("hide");
+  if (selectedOption === currentQuestion.correctAnswer) {
+    console.log("Correct!");
   } else {
-    gameOver();
+    console.log("Incorrect!");
   }
-}
 
-function setStatusClass(element, correct) {
-  clearStatusClass(element);
-  if (correct) {
-    element.classList.add("correct");
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < questions.length) {
+    displayQuestion();
   } else {
-    element.classList.add("wrong");
+    console.log("Quiz finished!");
   }
 }
-
-function clearStatusClass(element) {
-  element.classList.remove("correct");
-  element.classList.remove("wrong");
-}
-// countdown functions
 function countdown() {
   var timeInterval = setInterval(function () {
     startTime--;
@@ -128,7 +89,7 @@ function countdown() {
       questionContainer.classList.remove("hide");
       clearInterval(timeInterval);
       gameTime();
-      setNextQuestion();
+      displayQuestion();
     }
   }, 1000);
 }
@@ -142,18 +103,6 @@ function gameTime() {
     if (gameSeconds === 0) {
       timerEl.textContent = "";
       clearInterval(timeInterval);
-      gameOver();
     }
   }, 1000);
-}
-// end of game screen
-function gameOver() {
-  timerEl.textContent = "You did it!";
-  questionEl.textContent = "Enter your initials to join the leaderboard!";
-  var initialsInput = document.createElement("input");
-  questionContainer.appendChild(initialsInput);
-  var noThanks = document.createElement("button");
-  noThanks.textcontent = "No thanks";
-  questionContainer.appendChild(noThanks);
-  buttonContainer.classList.add("hide");
 }
